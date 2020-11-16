78f67025bc55c036f609df0f7bb6002ee2180192
Nattaphon <natlove.natlove202@gmail.com>
2020-11-16 19:42:26 +0700

    :art: Add ex2 11/16/2020 :doughnut:

---
+++ ex2.py
@@ -0,0 +1,11 @@
+## EX2 Comments and Pound Characters
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have like this.") # and the comment after this ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
